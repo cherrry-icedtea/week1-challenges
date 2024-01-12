0811d8455b73ca8d1008025c7bfb173cf7558562
cherrry-icedtea <156423901+cherrry-icedtea@users.noreply.github.com>
2024-01-12 13:58:50 +0000

    challenges
    completed challenge one which stores and returns all meals of the day labelled

---
+++ challenges.js
@@ -0,0 +1,18 @@
+
+// 1
+/* challenge one! create a program that stores what you eat today for breakfast
+lunch, and dinner, returns a string with all of them in it*/
+
+
+let mealStore = (meal1, meal2, meal3) => {
+    meal1 = ("breakfast:" + meal1)
+    meal2 = ("lunch:" + meal2)
+    meal3 = ("dinner:" + meal3)
+
+    return daysFood = `${meal1}, ${meal2}, ${meal3}`
+}
+
+let firstFood = "pancakes", secondFood = "blt", thirdFood =  "steakandchips";
+console.log(mealStore(firstFood, secondFood, thirdFood))
+
+
