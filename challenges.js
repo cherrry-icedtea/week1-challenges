416e56e4bbd0d9371b2dc3bda04ef80bc1a3ba4a
cherrry-icedtea <156423901+cherrry-icedtea@users.noreply.github.com>
2024-01-12 16:46:31 +0000

    fixed challenge 4
    still need to update challenge 3 but challenge 4 is working

--- challenges.js
+++ challenges.js
@@ -23,20 +23,57 @@ if div 5 return "buzz"
 if both, return fizzbuzz
 if neither return num*/
 
-const fbmes = ["Number chosen must be above 0", "fizz", "buzz", "fizz buzz"]
+// const fbmes = ["Number chosen must be above 0", "fizz", "buzz", "fizz buzz"]
 
-const fbz = (num1) => {
-    if (num1 <= 0) {
-        return fbmes[0]
-    } else if (((num1 % 3) === 0) && ((num1 % 5) ===0)) {
-        return fbmes[3]
-    } else if ((num1 % 3) === 0) {
-        return fbmes[1]
-    } else if ((num1 % 5) === 0) {
-        return fbmes[2]
-    } else {
-        return num1
+// const fbz = (num1) => {
+//     if (num1 <= 0) {
+//         return fbmes[0]
+//     } else if (((num1 % 3) === 0) && ((num1 % 5) ===0)) {
+//         return fbmes[3]
+//     } else if ((num1 % 3) === 0) {
+//         return fbmes[1]
+//     } else if ((num1 % 5) === 0) {
+//         return fbmes[2]
+//     } else {
+//         return num1
+//     }
+// }
+
+// console.log(fbz(15))
+
+
+// 3
+//DO AGAIN LATER DOESN'T WORK+
+
+// let checkEven = (num1, num2) => {
+
+//     if (typeof num1), {
+//         return answer = "make sure you input 2 numbers"
+//     }
+
+// }
+
+// console.log(checkEven(5, 6))
+
+
+
+// 4 
+/* random number generator that generates 6 random numbers between 1-50, add them to an array and return the array */
+const nums = [5];
+let numText = ""
+let numRand = () => {
+
+    for (let i = 0; i < (5); i++) {
+        nums.push(Math.floor((Math.random() * 50) + 1))
+        
+    //     if (i = 0) {
+    //         numText = nums[i]
+    //    } else {
+    //     numText = (numText + ", "+nums[i])
+    //    }
+
     }
+    return nums
 }
 
-console.log(fbz(15))
+console.log(numRand())
